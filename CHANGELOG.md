9d9b7d239bc482d587cc3d31b1c764e83ef04003
Giuseppe Sorrentino <Giuseppe@MACBOOK-AIR.station>
2025-04-27 10:49:11 +0200

    Preparazione release versione 1.0.6

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [1.0.6] - 2025-04-27
+
+### Correzioni
+- Ora è possibile risolvere l'importazione della liberia.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('CHANGELOG.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='LabToolbox',
+    version='1.0.6',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy', 'scipy', 'matplotlib.pyplot', 'statsmodels.api', 'math', 'lmfit', 'corner', 'emcee'
+    ],
+    author='Giuseppe Sorrentino',
+    author_email='sorrentinogiuse@icloud.com',
+    description="LabToolbox è una raccolta di strumenti per l'analisi e l'elaborazione di dati sperimentali in ambito scientifico.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    # url='https://github.com/tuo-username/tuo-pacchetto',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT',  # Aggiungi questa linea
+)
