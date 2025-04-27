33569a76bce93694f7c6faa58269028f7c27ae1e
Giuseppe Sorrentino <Giuseppe@MACBOOK-AIR.station>
2025-04-27 11:58:25 +0200

    Preparazione release

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
-with open('CHANGELOG.md', 'r') as f:
-    long_description = f.read()
+# Carica il contenuto del README e del CHANGELOG
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    readme_content = readme_file.read()
+
+with open("CHANGELOG.md", "r", encoding="utf-8") as changelog_file:
+    changelog_content = changelog_file.read()
+
+# Combina il contenuto dei due file
+long_description = readme_content + "\n\n" + "## Changelog\n\n" + changelog_content
 
 __version__ = '1.0.7'
 
@@ -9,17 +16,15 @@ setup(
     name='LabToolbox',
     version=__version__,
     packages=find_packages(),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description=long_description,  # Combina entrambi i contenuti
+    long_description_content_type="text/markdown",  # Se usi README.md
     install_requires=[
         'numpy', 'scipy', 'matplotlib.pyplot', 'statsmodels.api', 'math', 'lmfit', 'corner', 'emcee'
     ],
     author='Giuseppe Sorrentino',
     author_email='sorrentinogiuse@icloud.com',
     description="LabToolbox è una raccolta di strumenti per l'analisi e l'elaborazione di dati sperimentali in ambito scientifico.",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    # url='https://github.com/tuo-username/tuo-pacchetto',
+    url="https://github.com/giusesorrentino/LabToolbox",
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
