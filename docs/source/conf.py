21741da5c46b9f8f6da1b554753e9161db7de3a9
Giuseppe Sorrentino <166509923+giusesorrentino@users.noreply.github.com>
2025-05-10 13:54:33 +0200

    Add documentation folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,19 +19,27 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))  # o il path dove sta LabToolbox
+sys.path.insert(0, os.path.abspath('..'))  # o percorso alla tua libreria
+
+# Estensioni
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',   # supporto Google/Numpy style docstrings
-    'sphinx.ext.viewcode'
+    'sphinx.ext.napoleon',  # per Google/Numpy docstring
+    'sphinx.ext.viewcode',
+    'numpydoc',             # stile NumPy
 ]
+
+# Tema
+html_theme = 'sphinx_rtd_theme'
+
+# Opzioni per il tema
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+}
+
+# Percorso statico (se vuoi aggiungere CSS personalizzato)
+html_static_path = ['_static']
