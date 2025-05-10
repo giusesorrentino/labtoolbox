bbef229540b63efc60666b488f7c9fe7887dc3e4
Giuseppe Sorrentino <166509923+giusesorrentino@users.noreply.github.com>
2025-05-10 13:28:09 +0200

    Add documentation folder

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'LabToolbox'
+copyright = '2025, Giuseppe Sorrentino'
+author = 'Giuseppe Sorrentino'
+release = '3.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))  # o il path dove sta LabToolbox
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',   # supporto Google/Numpy style docstrings
+    'sphinx.ext.viewcode'
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,23 @@
+.. LabToolbox documentation master file, created by
+   sphinx-quickstart on Sat May 10 13:22:39 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+LabToolbox documentation
+========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Modules:
+
+   LabToolbox.utils
+   LabToolbox.fit
+   LabToolbox.stats
+   LabToolbox.linalg
+   LabToolbox.optics
+   LabToolbox.signals
+   LabToolbox.uncertainty
